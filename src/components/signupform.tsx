d17b41f4da95c0082eb4d71acb003da18e82c415
maithani74 <maithanir74@gmail.com>
2024-12-20 20:53:28 +0530

    changes

--- src/components/signupform.tsx
+++ src/components/signupform.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { nhost } from "../lib/nhost";
 import { signIn } from "next-auth/react";
 import bcrypt from "bcryptjs";
 import { ToastContainer, toast } from "react-toastify";
@@ -120,46 +119,7 @@ export function SignupForm() {
     }
   };
 
-  const handleGoogleLogin = async () => {
-    try {
-      const res = await nhost.auth.signIn({
-        provider: "google",
-      });
-
-      console.log(res)
-      if (res.error) {
-        toast.error(res.error.message, {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-        });
-      } else {
-        toast.success("Logged in with Google", {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-        });
-        setTimeout(() => {
-          window.location.href = "/";
-        }, 3000);
-      }
-    } catch (error: any) {
-      toast.error("Google login failed. Please try again.", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-      });
-    }
-  };
+  
 
   return (
     <>
@@ -220,15 +180,6 @@ export function SignupForm() {
               Login
             </Link>
           </div>
-          <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path
-                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                fill="currentColor"
-              />
-            </svg>
-            Login with Google
-          </Button>
         </CardContent>
       </Card>
     </>

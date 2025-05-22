e9e66da6fb1f43afeacd982ea0e2880eb7763baf
maithani74 <maithanir74@gmail.com>
2025-05-22 08:07:56 +0530

    edits

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -904,14 +904,7 @@ export default function Bookings() {
                         View All Locations
                       </button>
                       {/* You could add a notification signup or other CTA here */}
-                      <button
-                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-6 rounded-lg transition-all duration-300"
-                        onClick={() => {
-                          /* Maybe add to waitlist or notification */
-                        }}
-                      >
-                        Notify Me When Available
-                      </button>
+                      
                     </div>
                   </div>
                 </div>
